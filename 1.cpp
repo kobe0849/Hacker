b9a514c67a9b8dcba9436dfa0bb563e9476bb493
kobe20110849@gmail.com <feixia@mogujie.com>
2015-03-12 10:33:33 +0800

    first commit

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+// =====================================================================================
+// 
+//       Filename:  1.cpp
+// 
+//    Description:  
+// 
+//        Version:  1.0
+//        Created:  2015/03/12 10时33分06秒
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  feixia, 
+//        License:  Copyright © 2015 feixia. All Rights Reserved.
+// 
+// =====================================================================================
+#include<iostream>
